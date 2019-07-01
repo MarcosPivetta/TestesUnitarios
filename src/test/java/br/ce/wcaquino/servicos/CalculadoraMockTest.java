0102c85ea475ce7b7cf6cae3cc924f38db772940
Marcos Pivetta <marcospivetta.ti@gmail.com>
2019-07-01 19:12:47 -0300

    33. Capturando Argumentos

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class CalculadoraMockTest {
@@ -11,8 +12,10 @@ public class CalculadoraMockTest {
 	@Test
 	public void teste() {
 		Calculadora calculadora = Mockito.mock(Calculadora.class);
-		when(calculadora.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5);
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);
+		when(calculadora.somar(argumentCaptor.capture(), argumentCaptor.capture())).thenReturn(5);
 		
 		assertEquals(5, calculadora.somar(1, 100000));
+		System.out.println(argumentCaptor.getAllValues());
 	}
 }
